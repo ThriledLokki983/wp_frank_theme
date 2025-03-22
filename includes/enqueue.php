9f9a6396e074b4a223b72f0247fcbb4c8d556493
Gideon Nimoh <gideon.nimoh@pwc.com>
2025-03-22 15:34:07 +0100

    Added cache busting to the enque using chatGpt

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -3,18 +3,31 @@
 function frank_enqueue_assets()
 {
     // get version of theme and set it as asset version
-    $theme = wp_get_theme();
-    $version = $theme->version;
+    // $theme = wp_get_theme();
+    // $version = $theme->version;
 
-    // fonts
-    // wp_enqueue_style('myriad', get_theme_file_uri('/public/assets/fonts/Myriad-Pro-Regular.ttf'), false, $version);
+    // // fonts
+    // // wp_enqueue_style('myriad', get_theme_file_uri('/public/assets/fonts/Myriad-Pro-Regular.ttf'), false, $version);
 
-    // CSS
-    wp_enqueue_style('frank', get_theme_file_uri('/public/css/styles.css'), false, $version);
-    wp_enqueue_style('fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?ver=6.0.2', false, $version);
+    // // CSS
+    // wp_enqueue_style('frank', get_theme_file_uri('/public/css/styles.css'), false, $version);
+    // wp_enqueue_style('fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?ver=6.0.2', false, $version);
 
-    // JS
-    wp_enqueue_script('frank', get_theme_file_uri('/public/js/script.js'), false, $version);
+    // // JS
+    // wp_enqueue_script('frank', get_theme_file_uri('/public/js/script.js'), false, $version);
+
+    $theme_version = wp_get_theme()->get('Version');
+
+    // Path to your compiled CSS file
+    $css_file = get_template_directory() . '/public/css/styles.css';
+    $css_version = file_exists($css_file) ? filemtime($css_file) : $theme_version;
+
+    wp_enqueue_style('frank', get_template_directory_uri() . '/public/css/styles.css', [], $css_version);
+
+    $js_file = get_template_directory() . '/public/js/script.js';
+    $js_version = file_exists($js_file) ? filemtime($js_file) : $theme_version;
+
+    wp_enqueue_script('frank', get_template_directory_uri() . '/public/js/script.js', [], $js_version, true);
 }
 
 add_action('wp_enqueue_scripts', 'frank_enqueue_assets');
